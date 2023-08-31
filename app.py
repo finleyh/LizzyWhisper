f88fc59649f7edf6d73a320da8ca5c81f9864381
Billy thumper <daddyshark@Recon2.lan>
2023-08-30 23:34:24 -0400

    updated the code to clean up execution

--- app.py
+++ app.py
@@ -8,23 +8,17 @@ class Whisperer:
         config = configparser.ConfigParser()
         config.read(config_file)
         self.engine = engine
-        self.persona = config["API"]["PERSONA"]
-        self.api_key=config["API"]["KEY"]
+        self.persona = config["API"]["PERSONA"].strip()
+        openai.api_key=config["API"]["KEY"].strip()
 
     def generate_response(self, message):
-        full_prompt = f"{self.prompt}\n{message}" if self.prompt 
+        full_prompt = f"{self.persona}\n{message}" 
         return self._generate_response(full_prompt)
 
-    def _generate_response(self, prompt, max_tokens=-1): 
-        if max_tokens==-1:
-            response = openai.Completion.create(
-                engine=self.engine,
-                prompt=prompt
-            )
-        else:
-            response = openai.Completion.create(
-                engine=self.engine,
-                prompt=prompt,
-                max_tokens=max_tokens
-            )
-        return response.choices
+    def _generate_response(self, prompt, max_tokens=2048):
+        response = openai.Completion.create(
+            engine=self.engine,
+            prompt=prompt,
+            max_tokens=max_tokens
+        )
+        return response.choices[0].text
